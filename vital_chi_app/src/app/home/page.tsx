88a93e517d042a9345585fd8aa1f372b62b41596
morishun045 <morito.shun8051@mail.kyutech.jp>
2025-09-14 19:14:42 +0900

    hi日付表示

--- vital_chi_app/src/app/home/page.tsx
+++ vital_chi_app/src/app/home/page.tsx
@@ -2,12 +2,15 @@
 
 import './home.css';
 import HitPoint from '@/components/ui/hitpoint';
+import DateCount from '@/components/ui/datecount';
 
 export default function home () {
     return (
         <div className="home">
             <div>
                 <HitPoint />
+                <DateCount />
+
             </div>
         </div>
     );
---
+++ vital_chi_app/src/components/ui/datecount.tsx
@@ -0,0 +1,9 @@
+export default function DateCount(){
+    return (
+        <div className="h-15 w-30  bg-white/50 absolute bottom-0 m-4">
+            <div className="text-3xl m-4">
+                Date
+            </div>
+        </div>
+    );
+}
