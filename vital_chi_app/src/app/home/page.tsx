2120edaf70fea2dadbd230058334ba8246194dc3
morishun045 <morito.shun8051@mail.kyutech.jp>
2025-09-14 18:41:31 +0900

    hitpoint表示

--- vital_chi_app/src/app/home/page.tsx
+++ vital_chi_app/src/app/home/page.tsx
@@ -1,11 +1,13 @@
 /* デフォルトの画面 */
 
 import './home.css';
+import HitPoint from '@/components/ui/hitpoint';
 
 export default function home () {
     return (
         <div className="home">
             <div>
+                <HitPoint />
             </div>
         </div>
     );
